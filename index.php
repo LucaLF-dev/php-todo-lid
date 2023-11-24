a366a9a8698b97deaed6ad6b8044145bac156ba1
LucaLF-dev <laficaraluca@gmail.com>
2023-11-24 14:24:43 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/app.css">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <title>Do To List</title>
+</head>
+<body>
+    <div id="app">
+       <div class="container">
+        <h1 style="text-align: center;">{{title}}</h1>
+       </div>
+       <div class="container">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+       </div>
+    </div>
+    <script src="./js/app.js"></script>
+</body>
+</html>
